6ffcc4436adce39a1fef8f202adc3ebea2e5d202
Jin <celine.jkim@gmail.com>
2018-06-02 00:33:02 -0400

    html --> handlebars, DB modified

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 //mysql connection
 var mysql = require("mysql");
 
-<<<<<<< HEAD
 // DB connection (clearDB)
-=======
-DB connection (clearDB)
->>>>>>> 5363ba40dc648fe958b3f240f07d0a1de293953e
 var connection = mysql.createConnection({
     host : 'us-cdbr-iron-east-04.cleardb.net',
     user : 'b6633b71acf36b',
@@ -15,11 +11,7 @@ var connection = mysql.createConnection({
 
 //DB connection (mysql)
 //var connection = mysql.createConnection({
-<<<<<<< HEAD
 //    host : 'localhost',
-=======
-//   host : 'localhost',
->>>>>>> 5363ba40dc648fe958b3f240f07d0a1de293953e
 //    user : 'web',
 //    password: '1234',
 //    database: 'dogprofile'
@@ -35,23 +27,12 @@ var handlebarObj = '';
 var myhandlebarObj = '';
 
 
-//connection.query(queryString, function(err, res){
-//    if(err) {
-//        console.log(err);
-//    }
-//    //console.log(res);
-//
-//    //handlebar object
-//    handlebarObj = {dog_info: res};
-//})
-//
-//connection.end();
-
 //display
 var express = require("express");
 var app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(process.cwd() + '/public'));
 
 //set handlebars as the view engine
 var handlebars = require("express-handlebars");
@@ -63,65 +44,41 @@ app.engine("handlebars", handlebars({
 }));
 app.set("view engine", "handlebars");
 
+
+//index.html --> index.handlebars
 app.get("/", function(req, res){
     res.render("index");
 });
 
-app.get("/search", function(req, res){
-    
+//contact.html --> contact.handlebars
+app.get("/contact",function(req, res){
+    res.render("contact");
+});
+
+//login.html --> login.handlebars
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get('/searchme', function(req, res){
+  
     var myEmail = req.query.email;
     var myZipcode = req.query.zipcode;
-
+  
     //DB query
-    var queryString = 'SELECT * FROM ' + dbTable + ' WHERE email ="'+ myEmail + '" and zipcode = '+myZipcode + ';';
-    //console.log(queryString);
+    var queryString = 'SELECT * FROM ' + dbTable + ' WHERE first_name = "'+ myEmail+ '" and zipcode = '+ myZipcode + ';';
     connection.query(queryString, function(err, res){
-        if(err) {console.log(err);}
-        //handlebar object
-        myhandlebarObj = {myProfile: res};
+      if(err) {console.log(err);}
+      //handlebar object
+      myhbsObject = {myInfo: res};
     });
-    res.render("search", myhandlebarObj);
     
-});
+    //pass myhbsOjbect to profile handlebars
+  });
 
-app.get('/result',function(req,res){
-    var zipcode = parseInt(req.query.zipcode.substr(0,2));
-    var dogPersonality = req.query.dog_personality;
-    var dogGender = req.query.dog_gender;
-    var dogAge = parseInt(req.query.dog_age);
-    //var dogWeight = parseInt(req.query.dog_weight); 
 
-    var queryString = 'SELECT * FROM ' + dbTable;
-    if (zipcode){
-        queryString+=' WHERE zipcode LIKE "' + zipcode + '___"';
-    }
 
-    if (dogPersonality){
-        queryString+=' AND dog_personality = "' + dogPersonality + '"';
-    }
-
-    if (dogGender){
-        queryString+=' AND dog_gender = "' + dogGender + '"';
-    }
-
-    if (dogAge){
-        queryString+=' AND dog_age = "' + dogAge + '"';
-    }
-
-    // if (dogWeight){
-    //     queryString+=' AND dog_weight = "' + dogWeight + '"';
-    // }
-
-    //console.log(queryString);
-
-    connection.query(queryString, function(err,res){
-        if(err) {console.log(err);}
-
-        handlebarObj = {dog_profile: res};
-    });
-    res.render('result', handlebarObj)
-    
-});
+//signup.html --> create.handlebars
 
 app.get('/create', function(req, res){
     res.render('create');
@@ -155,12 +112,74 @@ app.post('/create', function(req, res){
 });
 
 
-//app.get("/profile", function(req, res){
-//    res.render("profile", handlebarObj);
-//});
-//
+// date.html --> search.handlebars
 
-//
+app.get('/search', function(req, res){
+    res.render('search');
+});
+
+app.get('/result',function(req,res){
+    var zipcode = parseInt(req.query.zipcode.substr(0,2));
+    var dogPersonality = req.query.dog_personality;
+    var dogGender = req.query.dog_gender;
+    var dogAge = parseInt(req.query.dog_age);
+    var dogWeight = parseInt(req.query.dog_weight); 
+
+    var queryString = 'SELECT * FROM ' + dbTable;
+    if (zipcode){
+        queryString+=' WHERE zipcode LIKE "' + zipcode + '___"';
+    }
+
+    if (dogPersonality){
+        queryString+=' AND dog_personality = "' + dogPersonality + '"';
+    }
+
+    if (dogGender){
+        queryString+=' AND dog_gender = "' + dogGender + '"';
+    }
+
+    if (dogAge){
+        queryString+=' AND dog_age = "' + dogAge + '"';
+    }
+
+    if (dogWeight){
+        queryString+=' AND dog_weight = "' + dogWeight + '"';
+    }
+
+    //console.log(queryString);
+
+    connection.query(queryString, function(err,res){
+        if(err) {console.log(err);}
+
+        handlebarObj = {dog_profile: res};
+    });
+    res.render('result', handlebarObj)
+    
+});
+
+
+//profile.html --> profile.handlebars
+
+app.get("/profile", function(req, res){
+    
+    //get these values from login pages
+    //for now they have test values
+    var myEmail = "mtwiggins0@parallels.com";
+    var myZipcode = "26925";
+    
+    //DB query
+    var queryString = 'SELECT * FROM ' + dbTable + ' WHERE email ="'+ myEmail + '" and zipcode = '+myZipcode + ';';
+    //console.log(queryString);
+    connection.query(queryString, function(err, res){
+        if(err) {console.log(err);}
+        //handlebar object
+        myhandlebarObj = {myProfile: res};
+    });
+    res.render("profile", myhandlebarObj);
+    
+});
+
+
 //app.get("/scheduler", function(req, res){
 //    res.render("scheduler");
 //});
